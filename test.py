bbd512e0bc85ec50e849c6d5f520583e7f4696be
tariyalhimanshu@gmail.com <tariyalhimanshu@gmail.com>
2024-11-27 17:30:30 +0530

    this is my first python file

---
+++ test.py
@@ -0,0 +1,3 @@
+The SDLC is a process that goes from planning to deployment and everything in between. It starts with the idea of what you want to build, moves on to research and design, then into coding and testing, and finally ends with deploying the product.
+
+A lot goes into each of these steps, which is why it’s important to have the right tools at your disposal. But what are those tools? And what do they do? Here are various tools to rule your software development life cycle.
