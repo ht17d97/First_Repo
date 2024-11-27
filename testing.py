5185908e1dcef8e719414379746705439b2c74cd
Himanshu Tariyal <tariyalhimanshu@gmail.com>
2024-11-27 16:57:37 +0530

    test

--- testing.py
+++
@@ -1,5 +0,0 @@
-"If you want to walk fast, walk alone. But if you want to walk far, walk together." This quote emphasizes the importance of collaboration and teamwork in achieving long-term success.
-
-"Take the stones people throw at you, and use them to build a monument." Tata's resilience and ability to turn adversity into opportunities is a testament to his character.
-
-"I admire people who are very successful. But if that success has been achieved through too much ruthlessness, then I may admire that person less." This quote highlights Tata's emphasis on ethical business practices and human values.
